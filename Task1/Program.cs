73b91fc97ba2019377bf156b0983826a1fac33f5
Alexei <alexej484@gmail.com>
2022-09-08 15:47:28 +0600

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿public class primer
+{
+    public static void Main()
+        {
+            Console.WriteLine("enter a three-digit number:");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+        }
+}
